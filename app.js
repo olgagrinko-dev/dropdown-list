608edd1b14eefae804ce64fddbf7cb0329ff8b6a
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-02-12 23:16:44 +0300

    popup connection

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// onlanguagechange
+const img = document.querySelector(`.dropdown`);
+let dl = document.querySelector(`.dropdown-list`);
+
+img.addEventListener(`click`, function () {
+
+    if (dl.style.display === `none`) {
+        dl.style.display = `flex`;
+        document.querySelector('.img').style = `background-image: url(./assece/arrow-down.svg)`;
+    } else {
+        dl.style.display = `none`;
+        document.querySelector(`.img`).style = `background-image: url(./assece/arrow-up.svg)`;
+    }
+})

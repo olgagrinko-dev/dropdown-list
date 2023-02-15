5d30f860623ab9747a9548b92b8bf0bbade6fa07
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-02-15 22:12:15 +0300

    project

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const img = document.querySelector(`.dropdown`);
-let dl = document.querySelector(`.dropdown-list`);
+const dl = document.querySelector(`.dropdown-list`);
+const p = document.querySelector(`.dropdown p`);
 
 img.addEventListener(`click`, function () {
-
     if (dl.style.display === `none`) {
         dl.style.display = `flex`;
         document.querySelector('.img').style = `background-image: url(./assece/arrow-down.svg)`;
@@ -10,4 +10,8 @@ img.addEventListener(`click`, function () {
         dl.style.display = `none`;
         document.querySelector(`.img`).style = `background-image: url(./assece/arrow-up.svg)`;
     }
+})
+
+dl.addEventListener(`click`, function (event) {    
+    p.textContent = event.target.textContent;
 })
